a84def64e93b7d31fadc2b7c82f089367afddc82
Mustafa Ilikkan <milikkan@yahoo.com>
2024-11-15 01:02:21 +0300

    complete footer compenent and refactor homepage

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
-import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
-import { Carousel } from "./layouts/HomePage/Carousel";
-import { Heros } from "./layouts/HomePage/Heros";
-import { LibraryServices } from "./layouts/HomePage/LibraryServices";
+import { ExploreTopBooks } from "./layouts/HomePage/components/ExploreTopBooks";
+import { Carousel } from "./layouts/HomePage/components/Carousel";
+import { Heros } from "./layouts/HomePage/components/Heros";
+import { LibraryServices } from "./layouts/HomePage/components/LibraryServices";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
+      <Footer />
     </div>
   );
-}
-
-export default App;
+};
